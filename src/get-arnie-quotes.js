338eff4bee538bc8d5ab30584d89eadace8eae39
Anant Asthana <anant.asty@gmail.com>
2022-02-18 02:24:36 +0000

    Added my solution to the smokeball test

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,12 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = urls.map(async url => {
+    const { status, body } = await httpGet(url)
+    const message = JSON.parse(body).message
+    return status === 200 ? {"Arnie Quote": message }: { "FAILURE": message }
+  })
+  return Promise.all(results)
 };
 
 module.exports = {
